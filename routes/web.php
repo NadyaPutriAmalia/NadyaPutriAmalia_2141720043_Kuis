e8926acd9d5c2596f00811bf0505dedfc798e64a
= NadyaPutriAmalia <= putrinadya754@gmail.com>
2023-05-10 12:31:02 +0700

    Jobsheet Auth

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,6 @@ Route::get('/', function () {
 Route::get('/welcome', function () {
     return view('welcome');
 });
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
